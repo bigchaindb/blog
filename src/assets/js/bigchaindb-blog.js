b42f384697737cb483259196175fee8706ef2537
Matthias Kretschmann <m@kretschmann.io>
2017-01-13 17:27:03 +0100

    handle image captions, closes #7

--- src/assets/js/bigchaindb-blog.js
+++ src/assets/js/bigchaindb-blog.js
@@ -6,14 +6,28 @@
 
 
 
-// (function ($, undefined) {
-//     'use strict';
-//
-//     var $document = $(document);
-//
-//     $document.ready(function () {
-//
-//
-//
-//     });
-// })(jQuery);
+(function ($, undefined) {
+    'use strict';
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+        // create image captions from alt attribute when present
+        // Note: Ghost adds 'Alt text' to all content images by default so check for that
+        $('.post-content img').each( function() {
+
+            if ( $(this).attr('alt') != 'Alt text') {
+                $(this).wrap(
+                    '<figure class="image"></figure>'
+                ).after(
+                    '<figcaption>' +
+                    $(this).attr(
+                        'alt') +
+                    '</figcaption>'
+                );
+            }
+        });
+
+    });
+})(jQuery);
